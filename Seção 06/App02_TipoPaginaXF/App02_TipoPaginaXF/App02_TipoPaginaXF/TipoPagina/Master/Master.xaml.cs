8fe703fec59fae14e133761961da688ff567f7f0
José Guilherme Maragno Luiz <joseguilherme_15@hotmail.com>
2018-05-10 14:16:15 -0300

    Seção 06 - Troca de Telas

---
+++ Seção 06/App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF/TipoPagina/Master/Master.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App02_TipoPaginaXF.TipoPagina.Master
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+
+	public partial class Master : MasterDetailPage
+	{
+		public Master ()
+		{
+			InitializeComponent ();
+		}
+
+        private void MudarPagina01(object sender, EventArgs args)
+        {
+            Detail = new NavigationPage(new Navigation.Pagina1());
+            IsPresented = false;
+        }
+
+        private void MudarPagina02(object sender, EventArgs args)
+        {
+            Detail = new Navigation.Pagina2();
+            IsPresented = false;
+        }
+	}
+}
